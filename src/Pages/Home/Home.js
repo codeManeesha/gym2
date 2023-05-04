aad9f55f764ecf10bd76faa3ec10e9c5681ccaf9
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-04 22:58:20 +0530

    final merge

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,8 +2,20 @@ import React from "react";
 import Background from "../../Components/Backround/Backround";
 import back from "../../images/Back1.jpg";
 import Expert from '../Home/expert/Expert'
+import { useNavigate } from "react-router-dom";
+import { isRefresh } from "../../Recoil";
+import { useRecoilValue } from "recoil";
+import { useEffect } from "react";
 
 const Home = () => {
+  const nav =useNavigate()
+  const Login = useRecoilValue(isRefresh)
+   useEffect(()=>{
+    if(!Login){
+      nav ("/")
+    }
+   },[])
+
   return (
     <div>
       <Background
