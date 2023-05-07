530c9ca5c6654bdcbf73c2d4a3732d2c677a407e
codeManeesha <maneeshasingh19jun@gmail.com>
2023-05-07 13:14:44 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import PricingSection from "./Pages/PricingSection/PricingSection";
+import TrainingSection from "./Pages/TrainingSection/Training";
+import Navbar from "./Navbar/Navbar";
+import Home from "./Pages/Home/Home";
+import AboutUs from "./Pages/About/About";
+import Login from "./Components/form/Login";
+import SignUp from "./Components/form/SignUp";
+import Footer from "./Components/Footer/Footer";
+import NoMatch from "./Pages/NoMatch";
 
-import './App.css';
-import { Route,Routes } from 'react-router-dom';
-import PricingSection from './Pages/PricingSection/PricingSection';
-import TrainingSection from './Pages/TrainingSection/Training';
-import Navbar from './Navbar/Navbar';
-import Home from './Pages/Home/Home';
-import AboutUs from './Pages/About/About'
-import Login from './Components/form/Login'
-import SignUp from './Components/form/SignUp'
-import Footer from './Components/Footer/Footer'
-import NoMatch from './Pages/NoMatch';
-
-import { useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo({ top: 0 });
   }, [location]);
-  const show = location.pathname ==='/' || location.pathname === '/about' || location.pathname === '/contact';
+  const show =
+    location.pathname === "/" ||
+    location.pathname === "/aboutUs" ||
+    location.pathname === "/PricingSection" ||
+    location.pathname === "/TrainingSection" ;
   return (
     <div className="App">
-  { show && <Navbar />}
-    <Routes>
-    
-  
-      <Route path='/'element={<Home/>}/> 
-      <Route path='/aboutUs'element={<AboutUs/>}/> 
-      <Route path='/PricingSection'element={<PricingSection/>}/> 
-      <Route path='/TrainingSection'element={<TrainingSection/>}/> 
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signUp' element={<SignUp/>}/>  
-      <Route path='*' element={<NoMatch/>}/>
-    
-      
-    </Routes>
-     {show && <Footer/>}
-   
-
-    
-     
+     {show && <Navbar />} 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/PricingSection" element={<PricingSection />} />
+        <Route path="/TrainingSection" element={<TrainingSection />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+      {show && <Footer />}
     </div>
   );
 }
